5cb9ff367928a3f9fea1cff50b115ab425213d87
Benny250 <Leninbq@gmail.com>
2019-02-23 18:06:37 +0100

    una linea mas

---
+++ PruebaGit/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PruebaGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primera prueba Git en .NET");
+            Console.WriteLine("Prueba de subida al repositorio");
+        }
+    }
+}
